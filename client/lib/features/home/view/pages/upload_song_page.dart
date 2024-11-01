118c81d6350a2a6fd6c385768f47a7cacf27d69c
Rupam058 <rupam.irp@gmail.com>
2024-11-01 17:37:28 +0530

    song playing on the miniplayer

--- client/lib/features/home/view/pages/upload_song_page.dart
+++ client/lib/features/home/view/pages/upload_song_page.dart
@@ -3,11 +3,12 @@ import 'dart:io';
 import 'package:client/core/theme/app_pallet.dart';
 import 'package:client/core/utils.dart';
 import 'package:client/core/widgets/custom_field.dart';
+import 'package:client/core/widgets/loader.dart';
 import 'package:client/features/home/view/widgets/audio_wave.dart';
+import 'package:client/features/home/viewmodel.dart/home_viewmodel.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UploadSongPage extends ConsumerStatefulWidget {
@@ -24,6 +25,8 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
 
   File? selectedImage;
   File? selectedAudio;
+
+  final formKey = GlobalKey<FormState>();
 
   void selectAudio() async {
     final pickedAudio = await pickAudio();
@@ -52,6 +55,8 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(homeViewModelProvider.select((value) => value?.isLoading == true));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Upload Song'),
@@ -59,92 +64,109 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
         elevation: 0,
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              if (formKey.currentState!.validate() && selectedAudio != null && selectedImage != null) {
+                ref.read(homeViewModelProvider.notifier).uploadSong(
+                      selectedAudio: selectedAudio!,
+                      selectedThumbnail: selectedImage!,
+                      songName: songNameController.text,
+                      artistName: artistController.text,
+                      selectedColor: selectedColor,
+                    );
+              } else {
+                showSnackBar(context, 'Missing Fields');
+              }
+            },
             icon: const Icon(Icons.check),
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: selectImage,
-                child: selectedImage != null
-                    ? SizedBox(
-                        height: 150,
-                        width: double.infinity,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.file(
-                            selectedImage!,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      )
-                    : DottedBorder(
-                        color: Pallete.borderColor,
-                        radius: const Radius.circular(15),
-                        borderType: BorderType.RRect,
-                        strokeWidth: 2,
-                        strokeCap: StrokeCap.round,
-                        dashPattern: const [10, 5],
-                        child: const SizedBox(
-                          height: 200,
-                          width: double.infinity,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.folder_open,
-                                size: 40,
+      body: isLoading
+          ? const Loader()
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: selectImage,
+                        child: selectedImage != null
+                            ? SizedBox(
+                                height: 150,
+                                width: double.infinity,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: Image.file(
+                                    selectedImage!,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              )
+                            : DottedBorder(
+                                color: Pallete.borderColor,
+                                radius: const Radius.circular(15),
+                                borderType: BorderType.RRect,
+                                strokeWidth: 2,
+                                strokeCap: StrokeCap.round,
+                                dashPattern: const [10, 5],
+                                child: const SizedBox(
+                                  height: 200,
+                                  width: double.infinity,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.folder_open,
+                                        size: 40,
+                                      ),
+                                      SizedBox(height: 15),
+                                      Text(
+                                        'Select the Thumbnail for your song',
+                                        style: TextStyle(fontSize: 15),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
-                              SizedBox(height: 15),
-                              Text(
-                                'Select the Thumbnail for your song',
-                                style: TextStyle(fontSize: 15),
-                              ),
-                            ],
-                          ),
-                        ),
                       ),
+                      const SizedBox(height: 40),
+                      selectedAudio != null
+                          ? AudioWave(path: selectedAudio!.path)
+                          : CustomField(
+                              hintText: 'Pick Song',
+                              controller: null,
+                              readOnly: true,
+                              onTap: selectAudio,
+                            ),
+                      const SizedBox(height: 20),
+                      CustomField(
+                        hintText: 'Song Name',
+                        controller: songNameController,
+                      ),
+                      const SizedBox(height: 20),
+                      CustomField(
+                        hintText: 'Artist',
+                        controller: artistController,
+                      ),
+                      const SizedBox(height: 20),
+                      ColorPicker(
+                        pickersEnabled: const {
+                          ColorPickerType.wheel: true,
+                        },
+                        color: selectedColor,
+                        onColorChanged: (Color color) {
+                          setState(() {
+                            selectedColor = color;
+                          });
+                        },
+                      )
+                    ],
+                  ),
+                ),
               ),
-              const SizedBox(height: 40),
-              selectedAudio != null
-                  ? AudioWave(path: selectedAudio!.path)
-                  : CustomField(
-                      hintText: 'Pick Song',
-                      controller: null,
-                      readOnly: true,
-                      onTap: selectAudio,
-                    ),
-              const SizedBox(height: 20),
-              CustomField(
-                hintText: 'Song Name',
-                controller: songNameController,
-              ),
-              const SizedBox(height: 20),
-              CustomField(
-                hintText: 'Artist',
-                controller: artistController,
-              ),
-              const SizedBox(height: 20),
-              ColorPicker(
-                pickersEnabled: const {
-                  ColorPickerType.wheel: true,
-                },
-                color: selectedColor,
-                onColorChanged: (Color color) {
-                  setState(() {
-                    selectedColor = color;
-                  });
-                },
-              )
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
---
+++ client/lib/features/home/viewmodel.dart/home_viewmodel.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:client/core/provider/current_user_notifier.dart';
+import 'package:client/core/utils.dart';
+import 'package:client/features/home/models/song_model.dart';
+import 'package:client/features/home/repositories/home_repository.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'home_viewmodel.g.dart';
+
+@riverpod
+Future<List<SongModel>> getAllSongs(GetAllSongsRef ref) async {
+  final token = ref.watch(currentUserNotifierProvider)!.token;
+  final res = await ref.watch(homeRepositoryProvider).getAllSongs(token: token);
+
+  return switch (res) {
+    Left(value: final l) => throw l.message,
+    Right(value: final r) => r,
+  };
+}
+
+@riverpod
+class HomeViewModel extends _$HomeViewModel {
+  late HomeRepository _homeRepository;
+  @override
+  AsyncValue? build() {
+    _homeRepository = ref.watch(homeRepositoryProvider);
+    return null;
+  }
+
+  Future<void> uploadSong({
+    required File selectedAudio,
+    required File selectedThumbnail,
+    required String songName,
+    required String artistName,
+    required Color selectedColor,
+  }) async {
+    state = const AsyncValue.loading();
+    final res = await _homeRepository.uploading(
+      selectedAudio: selectedAudio,
+      selectedThumbnail: selectedThumbnail,
+      songName: songName,
+      artistName: artistName,
+      hexCode: rgbToHex(selectedColor),
+      token: ref.watch(currentUserNotifierProvider)!.token,
+    );
+
+    final val = switch (res) {
+      Left(value: final l) => state = AsyncValue.error(l.message, StackTrace.current),
+      Right(value: final r) => state = AsyncValue.data(r),
+    };
+
+    print(val);
+  }
+}
